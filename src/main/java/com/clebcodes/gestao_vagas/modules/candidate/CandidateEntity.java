db04e11a757f688e42faef158574f0b6cf2c964c
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-06-10 20:23:27 -0300

    feat: adds validation to Candidate Body

---
+++ src/main/java/com/clebcodes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,25 @@
+package com.clebcodes.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class CandidateEntity {
+  private UUID id;
+  private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+  private String username;
+
+  @Email(message = "O campo [email] não deve conter espaço")
+  private String email;
+
+  @Length(min = 6)
+  private String password;
+  private String description;
+  private String curriculum;
+}
