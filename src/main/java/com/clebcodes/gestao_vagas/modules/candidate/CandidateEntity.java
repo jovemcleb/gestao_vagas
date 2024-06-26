1bcbb6841371a84068a98ac954727eab0588419a
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-06-26 17:24:19 -0300

    feat: implements creation of CandidateEntity

--- src/main/java/com/clebcodes/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/clebcodes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,26 @@
 package com.clebcodes.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @NotBlank()
@@ -24,4 +34,7 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
