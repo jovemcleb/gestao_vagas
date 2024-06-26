00c5df911fa63c1edea2bc171fd8804c92e416f6
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-06-26 17:56:00 -0300

    feat: implements CandidateRepository and UserFoundException

--- src/main/java/com/clebcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/clebcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package com.clebcodes.gestao_vagas.modules.candidate.controllers;
 
+import com.clebcodes.gestao_vagas.exceptions.UserFoundException;
 import com.clebcodes.gestao_vagas.modules.candidate.CandidateEntity;
+import com.clebcodes.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/candidates")
 public class CandidateController {
+  @Autowired
+  private CandidateRepository candidateRepository;
+
 
   @PostMapping("/")
-  public void  create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity  create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
+      throw new UserFoundException();
+    });
+    return this.candidateRepository.save(candidateEntity);
   }
 }
